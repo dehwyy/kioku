e6f4a608a7a56411ccef6a7a50db5cd2b1a220ff
dehwyy <91pony@mail.ru>
2023-01-18 05:28:29 +0300

    array randomizer func

--- f/src/jest/helpers.test.ts
+++ f/src/jest/helpers.test.ts
@@ -1,9 +1,18 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 import { expect } from "@jest/globals"
+import Randomizer from "../tools/Randomizer"
 describe("spec", () => {
   test("randomizer test", () => {
-    expect(1).toBe(1)
+    const random = new Randomizer(["10", "21", "31", "41", "51"])
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
+    console.log(random.getInfinityNextElement())
   })
 })
 
--- f/src/tools/Randomizer.ts
+++ f/src/tools/Randomizer.ts
@@ -1,9 +1,34 @@
 class Randomizer {
-  order = [] as number[]
-  constructor(private array: number) {}
+  private index = 0
+  private orderedArray = [] as string[]
+  constructor(private array: string[]) {}
 
-  private makeRandomOrder() {}
-  public *getElement() {}
+  private makeRandomOrder() {
+    for (const card of this.array) {
+      const randomNumber = Math.random()
+      if (randomNumber > 0.5) {
+        this.orderedArray.unshift(card)
+      } else {
+        this.orderedArray.push(card)
+      }
+    }
+  }
+  private *getElement() {
+    const element = this.orderedArray[this.index]
+    this.index++
+    yield element
+  }
+  private getNextElement() {
+    return this.getElement().next().value
+  }
+  public getInfinityNextElement() {
+    const element = this.getNextElement()
+    if (element) return element
+    this.index = 0
+    this.orderedArray = []
+    this.makeRandomOrder()
+    return this.getNextElement()
+  }
 }
 
 export default Randomizer
