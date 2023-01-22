3850e8e9b0e4b95a98cde707b0ebe927f55ed521
dehwyy <91pony@mail.ru>
2023-01-22 16:39:50 +0300

    remove swagger (not native support for gql(

--- b/src/main.ts
+++ b/src/main.ts
@@ -1,23 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { MainModule } from './main.module'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as dotenv from 'dotenv'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
 
 dotenv.config()
 const PORT = process.env.PORT || 2726
-// const swaggerConfig = new DocumentBuilder()
-//   .setTitle('NestJS App')
-//   .setDescription('description')
-//   .setVersion('1.0')
-//   .build()
 
 const start = async () => {
   try {
     const app = await NestFactory.create<NestExpressApplication>(MainModule)
-    // const doc = SwaggerModule.createDocument(app, swaggerConfig)
-    // SwaggerModule.setup('/docs', app, doc)
     app.useGlobalPipes(new ValidationPipe())
     await app.listen(PORT, () =>
       console.log(`http://localhost:${PORT}/graphql`),
