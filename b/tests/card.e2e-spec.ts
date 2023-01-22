a04dac8f5779ac45708f93d3d446375c9987df90
dehwyy <91pony@mail.ru>
2023-01-23 00:27:14 +0300

    card e2e tests

--- b/tests/card.e2e-spec.ts
+++ b/tests/card.e2e-spec.ts
@@ -1,19 +1,14 @@
 import request from 'supertest-graphql'
 import gql from 'graphql-tag'
-import { getModelToken } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
 import { INestApplication } from '@nestjs/common'
-import CardDB from '@src/card/models/card.schema'
 import { MainModule } from '@src/main.module'
+
+type cardResponse = Record<string, { _id: string }>
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
-  const cardModel = {
-    _id: 'someRandomString',
-    backface: 'backface111',
-    face: 'face',
-  }
-
+  let id: string
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [MainModule],
@@ -23,20 +18,51 @@ describe('AppController (e2e)', () => {
   })
 
   it('create card', async () => {
-    const { data } = await request(app.getHttpServer())
-      .mutate(
-        gql`
+    const { data } = await request<cardResponse>(app.getHttpServer())
+      .mutate(gql`
           mutation CreateCard($face: String!, $backface: String!) {
             createCard(backface: $backface, face: $face) {
               _id
               backface
               face
             }
-          }
-        `,
-      )
-      .variables({ face: 'face1231233', backface: 'backface' })
-    console.log(data)
-    return
+          }`).variables({ face: 'test', backface: 'test' }).expectNoErrors()
+    id = data.createCard._id
+    expect(id).toBeTruthy()
+  })
+  it('update card', async () => {
+    const { data } = await request<{updateCard: string}>(app.getHttpServer())
+    .mutate(gql`
+      mutation UpdateCard($backface: String!, $face: String!, $id: String!) {
+          updateCard(backface: $backface, face: $face, id: $id)
+      }
+    `).variables({face: "test1", backface: "test1", id}).expectNoErrors()
+    expect(data.updateCard).toMatch(/Modified/i)
+  })
+  it('get card', async () => {
+    const { data } = await request<cardResponse>(app.getHttpServer())
+    .query (gql`
+        query CreateCard($id: ID!) {
+            card(id: $id) {
+                _id
+                backface
+                face
+            }
+        }`).variables({id: id}).expectNoErrors()
+    id = data.card._id
+    expect(id).toBeTruthy()
+  })
+  it('delete card', async () => {
+    const { data } = await request<cardResponse>(app.getHttpServer())
+    .mutate(gql`
+        mutation deleteCard($id: ID!) {
+            deleteCard(id: $id) {
+                face
+                backface
+                _id
+            }
+        }
+    `).variables({id}).expectNoErrors()
+    expect(data.deleteCard).toBeTruthy()
   })
 })
